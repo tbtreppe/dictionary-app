0115f25db6c953a8f6a0cd2ced9f17a13c5a34e6
Tracey <ttrepper@yahoo.com>
2024-01-31 08:18:01 -0500

    add search form with alert

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import dictionary from "./dictionary-main-image.jpeg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={dictionary} className="Main-img img-fluid" alt="dictionary" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={dictionary}
+            className="Main-img img-fluid"
+            alt="dictionary"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="app-footer">
+          <small>Coded by Tracey Reis</small>
+        </footer>
+      </div>
     </div>
   );
 }
