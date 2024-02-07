ae71f0017428a0d9029791c6fd2c1473095ce002
Tracey <ttrepper@yahoo.com>
2024-02-06 21:34:22 -0500

    add footer links

--- src/App.js
+++ src/App.js
@@ -17,7 +17,12 @@ function App() {
           <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="app-footer">
-          <small>Coded by Tracey Reis</small>
+          <small>
+            This project was coded by Tracey Reis and is open sourced on
+            <a href="https://github.com/tbtreppe/dictionary-app"> GitHub</a> and
+            hosted on
+            <a href="https://adorable-selkie-1c71c3.netlify.app"> Netlify</a>
+          </small>
         </footer>
       </div>
     </div>
